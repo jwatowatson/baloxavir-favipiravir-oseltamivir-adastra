43df38204619f7b1c755bd0ed57500be3074af70
kantapong31 <44419764+kantapong31@users.noreply.github.com>
2026-02-18 14:23:07 +0700

    update sensitivity analysis plots

--- plot_sensitivity_ITT.R
+++ plot_sensitivity_ITT.R
@@ -1,13 +1,13 @@
-intervention = 'interim_firstResults'
+intervention = 'Final'
 ref_arm = 'No Study Drug'
 
-
+ref_code <- ifelse(ref_arm == "No Study Drug", "NSD", ref_arm)
 
 ff = list.files(
   'Rout/',
   pattern = paste0(
-    intervention, "_mITT_", "[0-9]+", "_vs_",
-    ifelse(ref_arm == "No Study Drug", "NSD", ref_arm), ".RData"
+    intervention, "_mITT(_[0-9]+)?_vs_", ref_code,
+    "\\.RData$"
   )
 )
 ff = ff[grep(pattern = 'model_fits_1_', x = ff)]
@@ -18,7 +18,7 @@ ff = paste0('Rout/', ff)
 
 #main_mod = 1
 model_cols = c("#1640D6", "#16B2D6")
-names(model_cols) = paste('model', 1:nrow(model_settings))
+names(model_cols) = paste('model', 1:2)#nrow(model_settings))
 
 effect_ests=list()
 for(i in 1:length(ff)){
@@ -32,61 +32,61 @@ for(i in 1:length(ff)){
 
 
 
-  effect_ests_plots <- NULL
-  trt_order2 <- trt_order[trt_order != ref_arm] %>% rev()
+effect_ests_plots <- NULL
+trt_order2 <- trt_order[trt_order != ref_arm] %>% rev()
+
+for(i in 1:length(effect_ests)){
+  effect_ests_plot <- as.data.frame(effect_ests[[i]])
+  effect_ests_plot <- exp(effect_ests_plot)
+  colnames(effect_ests_plot)[1:5] <- c("L95", "L80", "med", "U80", "U95")
+  effect_ests_plot$arm <- row.names(effect_ests_plot)
+  effect_ests_plot$arm <- as.factor(effect_ests_plot$arm)
+  effect_ests_plot$arm <- factor(effect_ests_plot$arm, levels = trt_order2)
   
-  for(i in 1:length(effect_ests)){
-    effect_ests_plot <- as.data.frame(effect_ests[[i]])
-    effect_ests_plot <- exp(effect_ests_plot)
-    colnames(effect_ests_plot)[1:5] <- c("L95", "L80", "med", "U80", "U95")
-    effect_ests_plot$arm <- row.names(effect_ests_plot)
-    effect_ests_plot$arm <- as.factor(effect_ests_plot$arm)
-    effect_ests_plot$arm <- factor(effect_ests_plot$arm, levels = trt_order2)
-    
-    effect_ests_plot$model <- names(effect_ests)[i]
-    effect_ests_plots <- rbind(effect_ests_plots, effect_ests_plot)
-  }
-  
-  effect_ests_plots$model <- factor(effect_ests_plots$model, levels = rev(names(model_cols)),
-                                    labels = rev(c("mITT population", "ITT population")))
-  
-  #Labeling reference arm
-  lab_ref <- ref_arm
-  #Labeling intervention arm
-  my.labs <- levels(effect_ests_plot$arm)
-  
-  title <- paste0("B) Estimated treatment effects relative to \n", tolower(lab_ref), " arm")
-  
-  names(model_cols) <- rev(levels(effect_ests_plots$model))
-  
-  G <- ggplot(effect_ests_plots, 
-              aes(x = arm, y = med, col = model, group = model)) +
-    geom_rect(aes(ymin = min(0.75, min(L95)-0.05), ymax = study_threshold, xmin = 0, xmax = length(my.labs)+1), fill = "#7D7C7C", alpha = 0.2, col = NA) +
-    geom_point(position = position_dodge(width = 0.5), size = 6) +
-    geom_errorbar(aes(x = arm, ymin = L95, ymax = U95),position = position_dodge(width = 0.5), width = 0, linewidth = 1.25) +
-    geom_errorbar(aes(x = arm, ymin = L80, ymax = U80),position = position_dodge(width = 0.5), width = 0, linewidth = 3) +
-    scale_color_manual(values = model_cols, name = NULL) +
-    coord_flip() +
-    theme_bw(base_size = 18) +
-    geom_hline(yintercept = 1, col = "red", linetype = "32") +
-    scale_y_continuous(
-      labels = formatter,
-      limits = c(min(0.75, min(effect_ests_plots$L95) - 0.05), max(effect_ests_plots$U95) + .25),
-      expand = c(0, 0),
-      breaks = (function(rng){
-        rng_pct <- rng * 100
-        seq(from = floor(rng_pct[1] / 20) * 20, to = ceiling(rng_pct[2] / 20) * 20, by = 20) / 100
-      })(c(min(0.75, min(effect_ests_plots$L95) - 0.05), max(effect_ests_plots$U95) + .25))
-    ) +
-    scale_x_discrete(labels= my.labs) +
-    ylab("Change in viral clearance rate (%)") +
-    xlab("") +
-    #ggtitle(title)  + 
-    theme( axis.title  = element_text(face = "bold"),
-           plot.title  = element_text(face = "bold"),
-           legend.position = "bottom",
-           legend.title = element_text(face = "bold"))
-  G
+  effect_ests_plot$model <- names(effect_ests)[i]
+  effect_ests_plots <- rbind(effect_ests_plots, effect_ests_plot)
+}
+
+effect_ests_plots$model <- factor(effect_ests_plots$model, levels = rev(names(model_cols)),
+                                  labels = rev(c("mITT population", "ITT population")))
+
+#Labeling reference arm
+lab_ref <- ref_arm
+#Labeling intervention arm
+my.labs <- levels(effect_ests_plot$arm)
+
+title <- paste0("B) Estimated treatment effects relative to \n", tolower(lab_ref), " arm")
+
+names(model_cols) <- rev(levels(effect_ests_plots$model))
+
+G <- ggplot(effect_ests_plots, 
+            aes(x = arm, y = med, col = model, group = model)) +
+  geom_rect(aes(ymin = min(0.75, min(L95)-0.05), ymax = study_threshold, xmin = 0, xmax = length(my.labs)+1), fill = "#7D7C7C", alpha = 0.2, col = NA) +
+  geom_point(position = position_dodge(width = 0.5), size = 6) +
+  geom_errorbar(aes(x = arm, ymin = L95, ymax = U95),position = position_dodge(width = 0.5), width = 0, linewidth = 1.25) +
+  geom_errorbar(aes(x = arm, ymin = L80, ymax = U80),position = position_dodge(width = 0.5), width = 0, linewidth = 3) +
+  scale_color_manual(values = model_cols, name = NULL) +
+  coord_flip() +
+  theme_bw(base_size = 18) +
+  geom_hline(yintercept = 1, col = "red", linetype = "32") +
+  scale_y_continuous(
+    labels = formatter,
+    limits = c(min(0.75, min(effect_ests_plots$L95) - 0.05), max(effect_ests_plots$U95) + .25),
+    expand = c(0, 0),
+    breaks = (function(rng){
+      rng_pct <- rng * 100
+      seq(from = floor(rng_pct[1] / 20) * 20, to = ceiling(rng_pct[2] / 20) * 20, by = 20) / 100
+    })(c(min(0.75, min(effect_ests_plots$L95) - 0.05), max(effect_ests_plots$U95) + .25))
+  ) +
+  scale_x_discrete(labels= my.labs) +
+  ylab("Change in viral clearance rate (%)") +
+  xlab("") +
+  #ggtitle(title)  + 
+  theme( axis.title  = element_text(face = "bold"),
+         plot.title  = element_text(face = "bold"),
+         legend.position = "bottom",
+         legend.title = element_text(face = "bold"))
+G
 
 
 png(plot_name, width = 15, height = 8, units = "in", res = 350)
